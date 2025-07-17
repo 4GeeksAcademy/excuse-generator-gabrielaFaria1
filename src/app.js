7431d2aa658a156e4429c0be6a2c683be79d69fe
gabrielafariac <126210298+gabrielafariac@users.noreply.github.com>
2025-07-17 01:00:45 +0000

    first

--- src/app.js
+++ src/app.js
@@ -6,6 +6,23 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  
+  let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+  let action = ['ate', 'peed', 'crushed', 'broke'];
+  let what = ['my homework', 'my phone', 'the car'];
+  let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+
+function randomWord (array){
+  let randomArrayIndex = Math.floor(Math.random() * array.length)
+  let randomArrayWord = array[randomArrayIndex];
+  return randomArrayWord
+}
+let RandomWho = randomWord(who);
+let RandomAction = randomWord(action);
+let RandomWhat = randomWord(what);
+let RandomWhen = randomWord(when);
+  
+document.querySelector('#excuse').innerHTML= RandomWho + " " + RandomAction + " " +RandomWhat + " "+ RandomWhen +" "
+  
 };
+
